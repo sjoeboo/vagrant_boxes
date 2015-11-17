67aca39ff92403c4332c0be0865c3704ebb71080
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2015-11-17 16:26:27 -0500

    fix name, add 6.7 puppet

---
+++ centos-6-7-puppet/scripts/puppet.sh
@@ -0,0 +1,3 @@
+yum -y localinstall https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+yum -y localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum -y install puppet-agent
