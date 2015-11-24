921ed2853d687c8a9028593a1b5ecb9f69457654
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2015-11-24 16:04:18 -0500

    add some more bits

--- centos-6-7-puppet/scripts/puppet.sh
+++ centos-6-7-puppet/scripts/puppet.sh
@@ -1,3 +1,4 @@
 yum -y localinstall https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
 yum -y localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-yum -y install puppet-agent
+yum -y install puppet-agent rubygems git
+gem install librarian-puppet
--- centos-7.1-puppet/scripts/puppet.sh
+++ centos-7.1-puppet/scripts/puppet.sh
@@ -1,3 +1,5 @@
 yum -y localinstall https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y install puppet-agent
+yum -y install puppet-agent rubygems git
+gem install librarian-puppet
+
